d101aa7b7b9ecf0539923edb59f7b15044586a02
Ali Samad <aliussamad2004@gmail.com>
2025-07-29 16:06:55 +0530

    Some New Leetcode Questions

--- LC_81.cpp
+++ LC_81.cpp
@@ -1,8 +1,7 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-
-bool search(vector<int>& nums, int target){
+bool search(vector<int> & nums, int target){
     int left = 0;
     int right = nums.size() - 1;
     while(left <= right){
@@ -18,30 +17,29 @@ bool search(vector<int>& nums, int target){
             if(nums[left] <= target && target < nums[mid]){
                 right = mid - 1;
             }
-            else{
+            else {
                 left = mid + 1;
             }
         }
         else{
-            if(nums[mid] < target && target <= nums[right]){
+            if(nums[right] < target && target <= nums[right]){
                 left = mid + 1;
             }
             else{
                 right = mid - 1;
             }
         }
-    } 
+    }
     return false;
 }
 int main(){
-    vector<int> nums  = {2,5,6,0,0,1,2};
-    int target = 0;
-    if(search(nums,target)){
-        cout << "Target Found! "<< endl;
+    vector<int> nums = {2,5,6,0,0,1,2};
+    int target = 2;
+    if (search(nums, target)){
+        cout << "Target Found! " << endl;
     }
-    else{
+    else {
         cout << "Target Not Found! " << endl;
     }
     return 0;
-
 }
