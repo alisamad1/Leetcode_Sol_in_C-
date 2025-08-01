ad1336ce0c03351e66f144b3ec19e4e039e93449
Ali Samad <aliussamad2004@gmail.com>
2025-08-01 08:11:25 +0530

    Integer to Roman

--- LC_12.cpp
+++ LC_12.cpp
@@ -1,3 +1,4 @@
+// we will be writing the code for Integer to Roman (Leetcode #12)
 string intToRoman(int num) {
     const vector<int> val = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     const vector<string> sym = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
